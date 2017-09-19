790a170cf769a63ad47ef8712e4958aa86e0e03e
Michael Gord <michael.l.gord@gmail.com>
2017-09-19 18:23:37 -0400

    add safemath + updates to contract

--- contracts/HireIco.sol
+++ contracts/HireIco.sol
@@ -1,140 +1,136 @@
-pragma solidity ^0.4.13;
-
-import 'zeppelin-solidity/contracts/token/PausableToken.sol';
-
-contract HireIco is PausableToken {
+pragma solidity ^0.4.14;
+import './SafeMath.sol';
+contract HireIco {
     using SafeMath for uint256;
-
-    string public name = "HireMatch Token";
-    string public symbol = "HIRE";
-    uint256 public decimals = 18;
-
-    address public wallet; // address where funds are collected
-
+    string public name = "HireMatch Token"; // Token Name
+    string public symbol = "HIRE"; // Token Symbol
+    address public creatorContract; // Address of the Owner.
+    uint256 public decimals = 18; // Number of decimal points to the Token. Make trading easier.
+    address public beneficiary; // address where funds will be transferred after the ICO
     uint256 public preSaleStartTimestamp; // preSale start timestamp
     uint256 public preSaleEndTimestamp; // preSale end timestamp
-
     uint256 public icoStartTimestamp; // ICO start timestamp
     uint256 public icoEndTimestamp; // ICO end timestamp
-
-    uint256 public totalRaised; // total amount of money raised in wei
-
+    uint256 public totalRaisedInPreSale; // total amount of money raised in wei during the Pre Sale
+    uint256 public totalRaisedInICO; //total amount of money raised in wei during the ICO
     bool public halted = false; //the owner address can set this to true to halt the crowdsale due to emergency
-
-    uint256 public preSaleEtherMaxCap; // should be specified as: 8000 * 1 ether
-    uint256 public icoEtherMaxCap; // should be specified as: 40000 * 1 ether
-    uint256 public rate = 1800; // standard HIRE/ETH rate
-
+    bool public ended = false; // the ICO has ended.
+    uint256 public constant preSaleEtherMaxCap = 8000 * 1 ether; // should be specified as: 8000 * 1 ether
+    uint256 public constant icoEtherMaxCap = 40000 * 1 ether; // should be specified as: 40000 * 1 ether
+    uint256 public constant totalTokenSupply = 10 ** 8 * 1 ether; // Total Token Supply
+    uint256 public constant totalTokenForSale = 78 ** 7 * 1 ether; // Total Available Supply
+    uint256 public constant preSaleRate = 2100; // standard HIRE/ETH rate during the Pre Sale
+    uint256 public constant icoRate = 1800; // standard HIRE/ETH rate during the ICO
+    mapping(address => uint256) balances; // Keeps track of all the purchases made by different addresses
     event Buy(address indexed recipient, uint256 weiAmount, uint256 tokens);
-    
-    // Unit tested
-    function HireIco(
-            address newOwner, 
-            uint256 newPreSaleStartTimestamp, 
-            address newWallet,
-            uint256 newPreSaleEtherMaxCap,
-            uint256 newIcoEtherMaxCap) {
-        require(newOwner != 0x0);
-        require(newWallet != 0x0);
-        require(newPreSaleStartTimestamp > now);
-        require(newPreSaleEtherMaxCap > 0);
-        require(newIcoEtherMaxCap > 0);
-        pause();
-        totalSupply = 0; // pre-sale and ICO will generate tokens
-        preSaleStartTimestamp = newPreSaleStartTimestamp;
-        preSaleEndTimestamp = preSaleStartTimestamp + 7 days;
-        preSaleEtherMaxCap = newPreSaleEtherMaxCap;
-        icoEtherMaxCap = newIcoEtherMaxCap;
-        wallet = newWallet;
-        transferOwnership(newOwner);
-    }
-
-    /// fallback function to buy tokens
-    function () nonHalted nonZeroPurchase acceptsFunds payable {
-        address recipient = msg.sender;
-        uint256 weiAmount = msg.value;
-
-        uint256 tokens = calculateTokens(weiAmount); // calculate new tokens amount to be created
-
-        balances[recipient] = balances[recipient].add(tokens);
-        totalSupply = totalSupply.add(tokens);
-        totalRaised = totalRaised.add(weiAmount);
-
-        forwardFundsToWallet();
-        Buy(recipient, msg.value, tokens);
-    }
-    
-    /// PreSale Bonuses:
-    /// - 1-7 day 20%
-    // Unit tested
-    function calculateTokens(uint256 weiAmount) constant returns(uint) {
-        uint256 defaultAllocation = weiAmount.mul(rate);
-        if (isPreSalePeriod()) {
-            if (now <= preSaleStartTimestamp + 7 days) {
-                return defaultAllocation.mul(120).div(100);
-            }
-        }
-        return defaultAllocation;
-    }
-
-    // Unit tested
-    modifier acceptsFunds() {
-        require(isPreSalePeriod() || isIcoPeriod());
-        require(!isMaxCapReached());
+    // Checks if the caller is the owner of the contract
+    modifier onlyOwner() {
+        require(msg.sender == creatorContract);
         _;
     }
-
-    function isPreSalePeriod() public constant returns(bool isPreSalePeriod) {
-        return now >= preSaleStartTimestamp
-            && now <= preSaleEndTimestamp
-            && totalRaised < preSaleEtherMaxCap;
+    // Pre-Requisites to ensure that funds are accepted in the right timeslot, i.e., ICO or Pre Sale
+    modifier acceptsFunds() {
+        require(isPreSalePeriod() || isIcoPeriod());
+        if (isPreSalePeriod()){
+            require(!isMaxCapReachedPreSale());
+        } else{
+            require(!isMaxCapReachedICO());
+        }
+        _;
     }
-
-    function isIcoPeriod() public constant returns(bool isIcoPeriod) {
-        return icoStartTimestamp != 0 // after preSale but still before ICO
-                && now >= icoStartTimestamp
-                && now <= icoEndTimestamp;
-    }
-
-    function isMaxCapReached() public constant returns(bool isMaxCapReached) {
-        return totalRaised >= icoEtherMaxCap;
-    }
-
-    // Unit tested
+    // To check that ICO is not halted by the Owner.
     modifier nonHalted() {
         require(!halted);
         _;
     }
-
-    // Unit tested
+    // To check that the ICO is halted
+    modifier ishalted() {
+        require(halted);
+        _;
+    }
+    // Make sure that the purchase amount != 0
     modifier nonZeroPurchase() {
         require(msg.value > 0);
         _;
     }
-
-    function forwardFundsToWallet() internal {
-        wallet.transfer(msg.value); // immediately send Ether to wallet address, propagates exception if execution fails
+    // Constructor to initialize some of the variables
+    function HireIco(
+            address _owner,
+            uint256 _preSaleStartTimestamp,
+            address _beneficiary) {
+        require(_owner != 0x0);
+        require(_beneficiary != 0x0);
+        require(_preSaleStartTimestamp > now);
+        creatorContract = _owner;
+        beneficiary = _beneficiary;
+        preSaleStartTimestamp = _preSaleStartTimestamp;
+        preSaleEndTimestamp = preSaleStartTimestamp + 7 days;
+        icoStartTimestamp = preSaleEndTimestamp + 1 days;
+        icoEndTimestamp = icoStartTimestamp + 15 days;
     }
-
-    // Unit tested
-    function setIcoDates(uint256 newIcoStartTimestamp, uint256 newIcoEndTimestamp) public onlyOwner {
-        require(newIcoStartTimestamp <= newIcoEndTimestamp);
-        if (0 == icoStartTimestamp) {
-            icoStartTimestamp = newIcoStartTimestamp;
+    /// fallback function to buy tokens
+    function () nonHalted nonZeroPurchase acceptsFunds payable {
+        require(!ended);
+        address recipient = msg.sender;
+        uint256 weiAmount = msg.value;
+        uint256 tokens = calculateTokens(weiAmount); // calculate new tokens amount to be created
+        if(isPreSalePeriod()) {
+            balances[recipient] = balances[recipient].add(tokens);
+            totalRaisedInPreSale = totalRaisedInPreSale.add(weiAmount);
+        } else if (isIcoPeriod()) {
+            balances[recipient] = balances[recipient].add(tokens);
+            totalRaisedInICO = totalRaisedInICO.add(weiAmount);
         }
-        if (0 == icoEndTimestamp) {
-            icoEndTimestamp = newIcoEndTimestamp;
-        }
+        Buy(recipient, msg.value, tokens);
     }
-
-    // Unit tested
+    // Auto calculated the number of tokens, based on the WEIs transferred
+    // Providing 20% bonus for 1000+ ether value of tokens
+    function calculateTokens(uint256 weiAmount) constant returns(uint256) {
+        uint256 defaultAllocation = weiAmount.mul(icoRate);
+        if (isPreSalePeriod()) {
+            defaultAllocation = weiAmount.mul(preSaleRate);
+            if(weiAmount == 1000 * 1 ether){
+                defaultAllocation = defaultAllocation.mul(120).div(100);
+            }
+        }
+        return defaultAllocation;
+    }
+    // Check if the Pre Sale is still in Progress
+    function isPreSalePeriod() public constant returns(bool preSalePeriod) {
+        return now >= preSaleStartTimestamp
+            && now <= preSaleEndTimestamp
+            && totalRaisedInPreSale <= preSaleEtherMaxCap;
+    }
+    // Check is the ICO is still in progress.
+    function isIcoPeriod() public constant returns(bool icoPeriod) {
+        return now >= icoStartTimestamp
+            && now <= icoEndTimestamp
+            && totalRaisedInICO <= icoEtherMaxCap;
+    }
+    // If the Maximum Cap is reached in Pre Sale
+    function isMaxCapReachedPreSale() public constant returns(bool maxCapReachedPreSale) {
+        return totalRaisedInPreSale >= preSaleEtherMaxCap;
+    }
+    // If the Maximum Cap is reached in ICO
+    function isMaxCapReachedICO() public constant returns(bool maxCapReachedICO) {
+        return totalRaisedInICO >= icoEtherMaxCap;
+    }
+    // Ending the fundraising after timestamp is reached or ICO limit has reached
+    function endFundRaising() onlyOwner returns (bool){
+        require(now >= icoEndTimestamp || isMaxCapReachedICO());
+        transferFundsToBeneficiary();
+        return true;
+    }
+    // transfer the funds to the beneficiary, after the ICO
+    function transferFundsToBeneficiary() internal {
+        beneficiary.transfer(totalRaisedInICO + totalRaisedInPreSale); // immediately send Ether to beneficiary address
+    }
+    // To halt the ICO incase of any discrepancy, only by Owner.
     function haltFundraising() public onlyOwner {
         halted = true;
     }
-
-    // Unit tested
+    // To resume the ICO incase of any discrepancy, only by Owner.
     function unhaltFundraising() public onlyOwner {
         halted = false;
     }
-    
 }
